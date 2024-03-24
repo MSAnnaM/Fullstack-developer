e4132e8dd400e939bf3a4705afc3de52f1a14d29
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-03-25 00:10:59 +0200

    done

---
+++ js/modal.js
@@ -0,0 +1,55 @@
+const openBtn = document.getElementById('contactBtn');
+const contactModal = document.getElementById('contactModal');
+const closeModal = document.getElementById('closeModal');
+const contactForm = document.getElementById('contactForm');
+
+
+function openModal() {
+    contactModal.classList.remove("is-hidden");
+  contactModal.classList.add("backdrop");
+  document.body.classList.add('modal-open');
+}
+
+function closeModalFunc() {
+    contactModal.classList.remove("backdrop");
+  contactModal.classList.add("is-hidden");
+  document.body.classList.remove('modal-open');
+}
+
+
+contactBtn.addEventListener('click', openModal);
+
+
+closeModal.addEventListener('click', closeModalFunc);
+
+window.addEventListener('click', (e) => {
+  if (e.target === contactModal) {
+    closeModalFunc();
+  }
+});
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    const email = contactForm.elements['email'].value;
+    const message = contactForm.elements['message'].value;
+    console.log(email, message);
+
+  const postData = {
+    email: email,
+    message: message,
+    };
+    console.log(postData);
+    fetch('https://api-server-c4rg.onrender.com/api/users/help', {
+  method: 'POST',
+  body: postData
+})
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+    contactForm.reset();
+    closeModalFunc();
+});
